1cf82415991ed4451d443132c8f1e0489cc077e9
ExpanseWong <ExpanseWong@users.noreply.github.com>
2019-06-11 10:07:48 +0800

    add some new files

---
+++ src/main/java/com/wonders/quartzlearning/QuartzlearningApplication.java
@@ -0,0 +1,13 @@
+package com.wonders.quartzlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzlearningApplication.class, args);
+    }
+
+}
